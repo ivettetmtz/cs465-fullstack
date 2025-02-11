56507d24fe46a66cf7b547464609aaf3c4e86a09
Ivette Trejo <ivette.trejo@snhu.edu>
2025-02-11 00:33:13 -0600

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var aboutRouter = require('./app_server/routes/about');
+var roomsRouter = require('./app_server/routes/rooms');
 var contactRouter = require('./app_server/routes/contact');
 var mealsRouter = require('./app_server/routes/meals');
 var newsRouter = require('./app_server/routes/news');
-var roomsRouter = require('./app_server/routes/rooms');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+// Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -27,14 +36,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/about', aboutRouter);
+app.use('/rooms', roomsRouter);
 app.use('/contact', contactRouter);
 app.use('/meals', mealsRouter);
 app.use('/news', newsRouter);
-app.use('/rooms', roomsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
